08df8e797dfeb15d53cd192c0cae001d4d379539
Martin-SVK-hub <hugofifa255@gmail.com>
2026-05-11 09:30:01 +0200

    Update code.py

--- code.py
+++ code.py
@@ -34,3 +34,65 @@ def Dobrý():
     return "<p>Dobrý!</p>" """
 <img src="https://www.tasteofhome.com/wp-content/uploads/2018/01/EXPS_TOHVP24_37188_MF_04_10_2.jpg" alt="Koktail">
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gg
+z = [5, 10, 15, 20, 25, 40, 1200]
+z_sorted = []
+
+def find_smallest(z):
+    smallest = float("inf")
+    for item in z:
+        if item < smallest:
+            smallest = item
+    return smallest
+
+while z:
+    smallest = find_smallest(z)
+    z_sorted.append(smallest)
+    z.remove(smallest)
+
+print(z_sorted)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+z = [5, 10, 15, 20, 25, 40, 1200]
+z_sorted = []
+
+def find_smallest(z):
+    smallest = z[0]   
+        if item < smallest:
+        smallest = item
+    return smallest
+
+while len(z) > 0:
+    smallest = find_smallest(z)
+    z_sorted.append(smallest)
+    z.remove(smallest)
+
+print(z_sorted)
